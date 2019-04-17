b06e77c398dde6f2967c2261d821ef5a44ccc51d
gcu.wupengjie <8426304410@qq.comm>
2019-04-18 00:12:31 +0800

    jie04/18

--- page/TeamCollaborationSystem/js/project/adduser.js
+++ page/TeamCollaborationSystem/js/project/adduser.js
@@ -1,1 +1,40 @@
-意外错误。未能在撤销／重做时从本地历史记录复原文件内容。
+function GetQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg); //search,查询？后面的参数，并匹配正则
+	if(r != null) return unescape(r[2]);
+	return null;
+}
+
+$(function() {
+	var projectId = GetQueryString("projectId");
+	
+	document.getElementById("projectId").value = projectId;
+
+});
+
+function addUser(){
+		var routeUrl = "http://127.0.0.1:8088/teamcollaborationsystem/userproject/addUser.action";
+		var paramObj = $("#addUserForm").serializeObject();
+		$.ajax({
+            type: 'POST',//方法类型
+            xhrFields: {withCredentials: true},
+            dataType: "json",//预期服务器返回的数据类型
+            contentType: "application/json",
+            data:JSON.stringify(paramObj),
+            async:false,
+            url: routeUrl,//url
+            success: function (result) {
+                console.log(result);//打印服务端返回的数据(调试用)
+                if (result.code == 0){
+                	window.location.href="homepage.html";
+                }else{
+                	if(result.code == "-1"){
+                		alert(result.msg);
+                	}
+                }
+            },
+            error : function() {
+                window.location.href = "error.html"
+            }
+        });
+    }
--- page/TeamCollaborationSystem/js/project/adduserjump.js
+++ page/TeamCollaborationSystem/js/project/adduserjump.js
@@ -1,1 +1,48 @@
-意外错误。未能在撤销／重做时从本地历史记录复原文件内容。
+function GetQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg); //search,查询？后面的参数，并匹配正则
+	if(r != null) return decodeURI(r[2]);
+	return null;
+}
+
+$(function() {
+	var projectId = GetQueryString("projectId");
+	var targetEmail = GetQueryString("targetEmail");
+	var role = GetQueryString("role");
+	
+	var addUserEntity = {"projectId":projectId,"targetEmail":targetEmail,"role":role};
+	
+	var routeUrl = "http://127.0.0.1:8088/teamcollaborationsystem/userproject/addUserJump.action";
+	$.ajax({
+            type: 'POST',//方法类型
+            xhrFields: {withCredentials: true},
+            dataType: "json",//预期服务器返回的数据类型
+            contentType: "application/json",
+            data:JSON.stringify(addUserEntity),
+            async:false,
+            url: routeUrl,//url
+            success: function (result) {
+                console.log(result);//打印服务端返回的数据(调试用)
+                if (result.code == 0){
+                	var targetProjectId = result.data;
+                	setTimeout("jumpPage()",5000);
+                	
+                }else{
+                	if(result.code == "-1"){
+                		alert(result.msg);
+                	}
+                }
+            },
+            error : function() {
+                window.location.href = "error.html"
+            }
+        });
+
+});
+
+
+function jumpPage(){
+	var projectId = GetQueryString("projectId");
+	alert("初始化信息成功，开始进入项目组吧");
+	window.location.href = "projectDetail.html?projectId= "+projectId;
+}
